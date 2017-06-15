130d4e5ec8db73f572888c9dbc469930dee8f9e2
grudus <jakgruda@gmail.com>
2017-06-15 22:01:11 +0200

    Change displayed time on time change

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const clock = document.getElementsByClassName('g-clock')['0'];
 const clockItems = document.getElementsByClassName('g-clock-item');
 const hours = ['12', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
 const minutes = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55'];
+const Type = {HOURS: 'hours', MINUTES: 'minutes'};
 
 let Clock = {
     size: {width: 0, height: 0},
     middle: {x: 0, y: 0},
     isMouseDown: false,
     selected: undefined,
+    type: Type.HOURS,
 
     calculateClockFace: () => {
         Clock.size.width = clock.clientWidth;
@@ -46,7 +48,12 @@ let Clock = {
 
         const index = Math.round(angle / 30) % 12;
         Clock.selected = clockItems[index];
-        Clock.selected.classList.add('g-selected')
+        Clock.selected.classList.add('g-selected');
+        Clock.updateTime(Clock.selected.innerText);
+    },
+
+    updateTime: newTime => {
+        (Clock.type === Type.HOURS ? headerHours : headerMinutes).innerText = newTime;
     }
 };
 
@@ -63,14 +70,16 @@ function toggleToMinutes() {
     headerHours.classList.remove('g-active');
     headerMinutes.classList.add('g-active');
     Clock.changeDisplayed(minutes);
-    Clock.calculateClockFace()
+    Clock.calculateClockFace();
+    Clock.type = Type.MINUTES;
 }
 
 function toggleToHours() {
     headerHours.classList.add('g-active');
     headerMinutes.classList.remove('g-active');
     Clock.changeDisplayed(hours);
-    Clock.calculateClockFace()
+    Clock.calculateClockFace();
+    Clock.type = Type.HOURS;
 }
 
 function toRadians(angle) {
