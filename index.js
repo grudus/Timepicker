8c194b8e1bc037cdc80943cb3e9c744dfeb16653
grudus <jakgruda@gmail.com>
2017-06-15 12:40:02 +0200

    Add simple js time toggle

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-/**
- * Created by jakub on 14.06.2017.
- */
+const headerMinutes = document.getElementsByClassName('g-minute')[0];
+const headerHours = document.getElementsByClassName('g-hour')[0];
+const clock = document.getElementsByClassName('g-clock')[0];
+
+(function () {
+    let date = new Date();
+    headerHours.innerText = date.getHours();
+    headerMinutes.innerText = date.getMinutes();
+    toggleToHours();
+})();
+
+function toggleToMinutes() {
+    headerHours.classList.remove('g-active');
+    headerMinutes.classList.add('g-active');
+    clock.innerText = "05 10 15 20 25 30 35 40 45 50 55 00"
+}
+
+function toggleToHours() {
+    headerHours.classList.add('g-active');
+    headerMinutes.classList.remove('g-active');
+    clock.innerText = "1 2 3 4 5 6 7 8 9 10 11 12"
+}
