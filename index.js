b43ad7f8e96121c5b2c16e8c04b322bfe5b7e55b
grudus <jakgruda@gmail.com>
2017-06-15 20:05:18 +0200

    Fix styling bug and refactor

--- index.js
+++ index.js
@@ -8,44 +8,39 @@ const minutes = ['00', '05', '10', '15', '20', '25', '30', '35', '40', '45', '50
 (function () {
     let date = new Date();
     headerHours.innerText = date.getHours();
-    headerMinutes.innerText = date.getMinutes();
+    headerMinutes.innerText = date.getMinutes() < 10
+        ? '0' + date.getMinutes() : date.getMinutes();
     calculateClockFace();
 })();
 
 function toggleToMinutes() {
     headerHours.classList.remove('g-active');
     headerMinutes.classList.add('g-active');
-    for (let i = 0; i < clockItems.length; i++)
-        clockItems[i].innerText = minutes[i];
+    toggleClock(minutes);
     calculateClockFace()
 }
 
 function toggleToHours() {
     headerHours.classList.add('g-active');
     headerMinutes.classList.remove('g-active');
-    changeClock(1, 12, 1);
-    for (let i = 0; i < clockItems.length; i++)
-        clockItems[i].innerText = hours[i]
+    toggleClock(hours);
     calculateClockFace()
 }
 
-function changeClock(start, end, skip) {
-    let current = start;
-    for (let i = 0; i < clockItems.length; i++) {
-        if (current <= end) {
-            clockItems[i].innerText = current;
-            current += skip;
-        }
-    }
+function toggleClock(array) {
+    for (let i = 0; i < clockItems.length; i++)
+        clockItems[i].innerText = array[i]
 }
 
 function calculateClockFace() {
-    const middleX = clock.clientWidth / 2;
-    const middleY = clock.clientHeight / 2;
-    const radius = middleX / 2 - 20;
+    const width = clock.clientWidth;
+    const height = clock.clientHeight;
+    const middleX = width / 2;
+    const middleY = height / 2;
+    const radius = width / 2 - 20;
     for (let i = 0; i < clockItems.length; i++) {
         const angle = toRadians(i * 30);
-        let item = clockItems[i];
+        const item = clockItems[i];
         const itemWidth = item.offsetWidth;
         const itemHeight = item.offsetHeight;
         item.style.left = ((middleX + Math.sin(angle) * radius) - itemWidth / 2) + 'px';
